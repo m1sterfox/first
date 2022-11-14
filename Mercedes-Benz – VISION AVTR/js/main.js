8cc087137c93720948655072fce5b8d4f5cb39e3
Aleksander Lisovsky <aolisovsky@gmail.com>
2022-11-14 20:51:50 +0200

    added adaptive for mobile devices

--- Mercedes-Benz – VISION AVTR/js/main.js
+++ Mercedes-Benz – VISION AVTR/js/main.js
@@ -5,5 +5,14 @@ $(function(){
 		variableWidth: true,
 		prevArrow: '<img class="arrow arrow-left" src="images/Arrow-left.svg" alt="arrow left">',
 		nextArrow: '<img class="arrow arrow-right" src="images/Arrow-right.svg" alt="arrow right">',
+		responsive: [
+			{
+				breakpoint: 361,
+				settings: {
+					variableWidth: false,
+					slidesToShow: 1,
+				}
+			}
+		]
 	});
 });
